106510dfdae7637fff9a5a004b46f2c4927bb4cb
dmfrpro <dmfr2021y@gmail.com>
2026-03-14 02:36:25 +0300

    docker: add lazydocker package
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- modules/home/docker.nix
+++ modules/home/docker.nix
@@ -5,4 +5,26 @@
     docker
     docker-credential-helpers
   ];
+
+  programs.lazydocker = {
+    enable = true;
+    settings = {
+      customCommands = {
+        containers = [
+          {
+            name = "bash";
+            attach = true;
+            command = "docker exec -it {{ .Container.ID }} bash";
+            serviceNames = [ ];
+          }
+          {
+            name = "sh";
+            attach = true;
+            command = "docker exec -it {{ .Container.ID }} sh";
+            serviceNames = [ ];
+          }
+        ];
+      };
+    };
+  };
 }
