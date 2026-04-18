1fa2a0821f78fa03d6dd8a1879d01ed83fcb6cbe
dmfrpro <dmfr2021y@gmail.com>
2026-04-18 17:27:19 +0300

    kernel: remove redundant asus patches
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- hosts/zephyrus/boot/kernel.nix
+++ hosts/zephyrus/boot/kernel.nix
@@ -1,18 +1,8 @@
 { pkgs, ... }:
 
-let
-  asusConfig = {
-    name = "asus-linux";
-    patch = null;
-    extraConfig = ''
-      ASUS_ARMOURY m
-    '';
-  };
-in
 {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelPatches = [ asusConfig ];
 
     kernelModules = [
       "nvme"
