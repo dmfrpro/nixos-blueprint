eabf8b3c68f96b125a8dc2c21c93ea650780e925
dmfrpro <dmfr2021y@gmail.com>
2026-05-11 12:49:36 +0300

    zephyrus: switch to adios scheduler
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- hosts/zephyrus/filesystem.nix
+++ hosts/zephyrus/filesystem.nix
@@ -5,4 +5,10 @@
     enable = true;
     interval = "weekly";
   };
+
+  # Adios scheduler
+  services.udev.extraRules = ''
+    ACTION!="remove", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="adios"
+  '';
+
 }
