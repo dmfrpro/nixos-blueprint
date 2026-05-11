125415a8d8961510e931bd295646c03d6ab33e4e
dmfrpro <dmfr2021y@gmail.com>
2026-05-11 13:32:11 +0300

    mtkclient: install icon
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- modules/home/flashing.nix
+++ modules/home/flashing.nix
@@ -1,11 +1,35 @@
 { pkgs, ... }:
 
-{
-  home.packages =
-    with pkgs;
-    [
-      android-tools
-      picocom
-      mtkclient
+let
+  mtk = pkgs.mtkclient.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.hicolor-icon-theme ];
+
+    desktopItems = [
+      (pkgs.makeDesktopItem {
+        name = "mtkclient";
+        desktopName = "MTKClient";
+        comment = "Mediatek Flash and Repair Utility";
+        exec = "mtk_gui";
+        icon = "mtkclient";
+        categories = [ "Development" ];
+      })
     ];
+
+    postInstall = (old.postInstall or "") + ''
+      for size in 32 64 256 512; do
+        install -Dm644 $src/mtkclient/gui/images/logo_512.png \
+          "$out/share/icons/hicolor/''${size}x''${size}/apps/mtkclient.png"
+      done
+
+      mkdir -p $out/share/mtkclient/images
+      cp $src/mtkclient/gui/images/*.png $out/share/mtkclient/images/
+    '';
+  });
+in
+{
+  home.packages = with pkgs; [
+    android-tools
+    picocom
+    mtk
+  ];
 }
