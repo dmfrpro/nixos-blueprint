eb04781af6061ebf8ef833d931dde6a38df6ea8d
dmfrpro <dmfr2021y@gmail.com>
2026-06-06 22:01:10 +0300

    shell: remove heavy nix-index
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -58,12 +58,6 @@
     enableZshIntegration = true;
   };
 
-  programs.nix-index = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-  };
-
   programs.zoxide = {
     enable = true;
     enableBashIntegration = true;
